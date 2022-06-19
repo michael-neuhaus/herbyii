905b779736c2e73b8a07ad8963eec784f6b45603
michael-neuhaus <90478136+michael-neuhaus@users.noreply.github.com>
2022-06-19 21:55:34 +0200

    Add files via upload

---
+++ sensor_data.js
@@ -0,0 +1,45 @@
+const sensor_data =
+{
+      "herby_details": [
+            {
+                  "moisture": 1707,
+                  "light": 482,
+                  "temp": 0.0,
+                  "humi": 0.0,
+                  "date": "06/16/22",
+                  "time": "17:21:52"
+            },
+            {
+                  "moisture": 1715,
+                  "light": 480,
+                  "temp": 0.0,
+                  "humi": 0.0,
+                  "date": "06/16/22",
+                  "time": "17:22:00"
+            },
+            {
+                  "moisture": 1727,
+                  "light": 484,
+                  "temp": 0.0,
+                  "humi": 0.0,
+                  "date": "06/16/22",
+                  "time": "17:22:08"
+            },
+            {
+                  "moisture": 1718,
+                  "light": 479,
+                  "temp": 0.0,
+                  "humi": 0.0,
+                  "date": "06/16/22",
+                  "time": "17:22:17"
+            },
+            {
+                  "moisture": 1708,
+                  "light": 479,
+                  "temp": 0.0,
+                  "humi": 0.0,
+                  "date": "06/16/22",
+                  "time": "17:22:25"
+            }
+      ]
+}
