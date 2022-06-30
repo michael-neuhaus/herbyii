5b9a9ce8cb2e1e734f89ba924abdbec98745ad92
root <root@raspberrypi>
2022-06-30 16:57:13 +0200

    auto commit

--- sensor_data.js
+++ sensor_data.js
@@ -1,1 +1,1 @@
-const sensor_data ={'herby_details': [{'moisture': 1651, 'light': 514, 'temp': 0.0, 'humi': 0.0, 'date': '06/30/22', 'time': '16:57:06'}]}
+const sensor_data ={'herby_details': [{'moisture': 1651, 'light': 514, 'temp': 0.0, 'humi': 0.0, 'date': '06/30/22', 'time': '16:57:06'}, {'moisture': 1663, 'light': 547, 'temp': 0.0, 'humi': 0.0, 'date': '06/30/22', 'time': '16:57:09'}]}
