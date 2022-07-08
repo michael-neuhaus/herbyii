6466faba698f0ec60617dcebf74c3be49df8bb17
michael-neuhaus <90478136+michael-neuhaus@users.noreply.github.com>
2022-07-08 13:25:53 +0200

    Update sensor_data.js

--- sensor_data.js
+++ sensor_data.js
@@ -1,1 +1,1 @@
-const sensor_data = {u'herby_details': [{'temp': 25, 'light': 608, 'humi': 95, 'moisture': 1675, 'time': '14:59:13', 'date': '07/07/22'}]}
+const sensor_data = {'herby_details': [{'temp': 25, 'light': 608, 'humi': 95, 'moisture': 1675, 'time': '14:59:13', 'date': '07/07/22'}]}
