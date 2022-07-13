d53fe879eb09f030e41063ba5b808b63ab7b3919
michael-neuhaus <90478136+michael-neuhaus@users.noreply.github.com>
2022-07-13 11:45:35 +0200

    Update value-moisture.js

--- js/value-moisture.js
+++ js/value-moisture.js
@@ -4,7 +4,7 @@ document.getElementById('TDS-value').innerText = current_moist
 
 // color the value box green or red depenening acceptable range
 function color() {
-  if (current_moist >= 1000 && current_moist <= 2000) {
+  if (current_moist >= 500 && current_moist <= 1700) {
     document.getElementById('TDS-box').style.backgroundColor = "#6BB834"
     document.getElementById('TDS-box').style.color = "#1C370A"
     document.getElementById('TDS-box').style.boxShadow = "5px 10px 30px #6BB834"
