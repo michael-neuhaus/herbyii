b0324c3ab7a2f8ef218532f12c6906901ae17564
joykrupinski <joy.krupinski@github.com>
2022-07-21 14:55:23 +0200

    auto push

--- sensor_data.js
+++ sensor_data.js
@@ -1,1 +1,1 @@
-const sensor_data ={'herby_details': [{'tds': 557, 'light': 611, 'temperature': 25, 'humidity': 95, 'date': '07/21/22', 'time': '14:54:41'}]}
+const sensor_data ={'herby_details': [{'tds': 553, 'light': 610, 'temperature': 25, 'humidity': 95, 'date': '07/21/22', 'time': '14:55:20'}]}
